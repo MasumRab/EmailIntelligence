34474b0a210117f8bc1766cec6d3643341e94587
Masum Rab <masum.rab@gmail.com>
2025-11-06 18:08:12 +1100

    Fix: Prevent install-hooks.sh --help from exiting parent script

--- scripts/install-hooks.sh
+++ scripts/install-hooks.sh
@@ -58,7 +58,7 @@ parse_install_args() {
             --help|-h)
                 echo "Usage: $0 [--force] [--verbose]"
                 echo "Install Git hooks from orchestration-tools branch"
-                exit 0
+                return 0
                 ;;
             *)
                 log_error "Unknown option: $1"
